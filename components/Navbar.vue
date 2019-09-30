c71b6a8f7965e56d9f04f5fe2486e8d849f5755e
Imani Dap <imani.dap@gmail.com>
2019-09-30 23:55:39 +0200

    chore(navbar): add static structure and base styling

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,9 +1,41 @@
 <template>
-  <ul></ul>
+    <header>
+      <nav>
+        <ul>
+          <li><h4>Imani</h4></li>
+          <li>projects</li>
+          <li>about</li>
+          <li>writing</li>
+        </ul>
+      </nav>
+    </header>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss" >
+@import "~/assets/_variables.scss";
+
 ul {
-  height: 32px;
+  display: flex;
+  flex-direction: row; 
+  align-items: baseline;
+  justify-content: flex-end;
+  margin-right: 32px; 
+
+  h4 {
+    margin: 0;
+  }
+
+  li {
+    font-family: $font-family-heading;
+    padding: 16px;
+
+    &:first-child {
+      padding-left: 64px;
+      padding-top: 32px; 
+      margin-right: auto;
+    }
+  }
+
+  
 }
 </style>
